f6efa3aff04150a794408abcae8db1fc6d40766b
fisher310 <lihailong699@gmail.com>
2020-03-03 09:53:54 +0800

    add some solution

---
+++ src/p206/Solution.java
@@ -0,0 +1,42 @@
+package p206;
+
+import com.sun.org.apache.xpath.internal.WhitespaceStrippingElementMatcher;
+
+public class Solution {
+
+  public ListNode reverseList(ListNode head) {
+
+    ListNode pre = null, current, next;
+    current = head;
+    while (current != null) {
+      next = current.next;
+      current.next = pre;
+      pre = current;
+      current = next;
+    }
+    return pre;
+  }
+
+  public static void main(String[] args) {
+    ListNode head = new ListNode(1);
+    ListNode last = head;
+    for (int i = 2; i < 6; i++) {
+      last.next = new ListNode(i);
+      last = last.next;
+    }
+    printNode(head);
+
+    ListNode reverseHead = new Solution().reverseList(head);
+
+    printNode(reverseHead);
+  }
+
+  private static void printNode(ListNode head) {
+    ListNode current = head;
+    while (current != null) {
+      System.out.print(current.val + "->");
+      current = current.next;
+    }
+    System.out.println("null");
+  }
+}
---
+++ src/p599/Solution.java
@@ -0,0 +1,63 @@
+package p599;
+
+import java.util.*;
+
+public class Solution {
+  public String[] findRestaurant(String[] list1, String[] list2) {
+    /*    int min = Integer.MAX_VALUE;
+    Set<String> result = new HashSet<>();
+
+    for (int i = 0; i < list1.length; i++) {
+      for (int j = 0; j < list2.length; j++) {
+        if (list1[i].equals(list2[j])) {
+          int indexSum = i + j;
+          if (indexSum < min) {
+            result.clear();
+            min = indexSum;
+            result.add(list1[i]);
+          } else if (indexSum == min) {
+            result.add(list1[i]);
+          }
+        }
+      }
+    }
+
+    return result.toArray(new String[] {});*/
+
+    int minIndex = Integer.MAX_VALUE;
+    Set<String> result = new HashSet<>();
+    Map<String, Integer> map = new HashMap<>();
+    for (int i = 0; i < list1.length; i++) {
+      map.put(list1[i], i);
+    }
+
+    for (int j = 0; j < list2.length; j++) {
+      Integer index1 = map.get(list2[j]);
+      if (null != index1) {
+        int newIndex = index1 + j;
+        if (newIndex < minIndex) {
+          result.clear();
+          result.add(list2[j]);
+          minIndex = newIndex;
+        } else if (newIndex == minIndex) {
+          result.add(list2[j]);
+        }
+      }
+    }
+    return result.toArray(new String[] {});
+  }
+
+  public static void main(String[] args) {
+    //    String[] list1 = new String[] {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+    //    String[] list2 =
+    //        new String[] {"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse",
+    // "Shogun"};
+
+    String[] list1 = new String[] {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+    String[] list2 = new String[] {"KFC", "Shogun", "Burger King"};
+
+    String[] result = new Solution().findRestaurant(list1, list2);
+
+    System.out.println(Arrays.toString(result));
+  }
+}
