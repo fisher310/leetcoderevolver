9709a64cae784fbf3add0e1bd6749f5eee767185
fisher310 <lihailong699@gmail.com>
2020-03-23 08:46:08 +0800

    新增数字解法

--- src/p9/Solution.java
+++ src/p9/Solution.java
@@ -2,19 +2,30 @@ package p9;
 
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
+        if (x < 0) return false;
 
-        String a = String.valueOf(x);
-        char[] ch = a.toCharArray();
-        for (int i = 0, j = ch.length - 1; i < j; i++, j--) {
-            if (ch[i] != ch[j]) {
-                return false;
-            }
+        int real = x;
+        int num = 0;
+        while (x != 0) {
+            num = num * 10 + x % 10;
+            x = x / 10;
         }
-        return true;
+        return real == num;
     }
+//    public boolean isPalindrome(int x) {
+//        if (x < 0) {
+//            return false;
+//        }
+//
+//        String a = String.valueOf(x);
+//        char[] ch = a.toCharArray();
+//        for (int i = 0, j = ch.length - 1; i < j; i++, j--) {
+//            if (ch[i] != ch[j]) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
     public static void main(String[] args) {
         Solution s = new Solution();
