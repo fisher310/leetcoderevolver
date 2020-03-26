4accf816570099d7518d94925ca72195a3abc73d
fisher310 <lihailong699@gmail.com>
2020-03-26 12:01:20 +0800

    修改为分治法

--- src/main/java/p23/Solution.java
+++ src/main/java/p23/Solution.java
@@ -2,28 +2,36 @@ package p23;
 
 import util.ListNode;
 
-import java.util.PriorityQueue;
-
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (null == lists || lists.length == 0) {
             return null;
         }
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        return merge(lists, 0, lists.length - 1);
+    }
 
-        for (ListNode node : lists) {
-            while (node != null) {
-                heap.add(node.val);
-                node = node.next;
-            }
+    ListNode merge(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
         }
-
+        int mid = (start + end) / 2;
+        ListNode l1 = merge(lists, start, mid);
+        ListNode l2 = merge(lists, mid + 1, end);
         ListNode head = new ListNode(0);
         ListNode current = head;
-        while (!heap.isEmpty()) {
-            current.next = new ListNode(heap.poll());
-            current = current.next;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+                current = current.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+                current = current.next;
+            }
         }
+        if (l1 != null) current.next = l1;
+        if (l2 != null) current.next = l2;
         return head.next;
     }
 }
