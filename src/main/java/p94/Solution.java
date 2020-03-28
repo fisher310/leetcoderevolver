2e014b0b1dc6c4bab684e3f88e59c6edfcc6fa2c
fisher310 <lihailong699@gmail.com>
2020-03-28 09:46:40 +0800

    add some solution

--- src/main/java/p94/Solution.java
+++ src/main/java/p94/Solution.java
@@ -3,25 +3,26 @@ package p94;
 import util.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        inOrder(ans, root);
+
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            ans.add(curr.val);
+            curr = curr.right;
+        }
         return ans;
     }
 
-    private void inOrder(List<Integer> list, TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        if (node.left != null) {
-            inOrder(list, node.left);
-        }
-        list.add(node.val);
-        if (node.right != null) {
-            inOrder(list, node.right);
-        }
-    }
 }
