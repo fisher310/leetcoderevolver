e79d72bb9c6743beee1e915b174f3889d1b547d3
fisher310 <lihailong699@gmail.com>
2020-03-30 15:23:35 +0800

    改为迭代的方式前序遍历

--- src/main/java/p144/Solution.java
+++ src/main/java/p144/Solution.java
@@ -3,12 +3,28 @@ package p144;
 import util.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 二叉树的前序遍历
+ * 尝试使用迭代方式
+ */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        preorder(root, ans);
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                ans.add(curr.val);
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            curr = curr.right;
+        }
+
         return ans;
     }
 
@@ -18,5 +34,11 @@ class Solution {
 
         preorder(node.left, list);
         preorder(node.right, list);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Integer> ans = s.preorderTraversal(TreeNode.createTreeNode(new Integer[]{1, 5, 2, 3, 4}));
+        System.out.println(ans);
     }
 }
