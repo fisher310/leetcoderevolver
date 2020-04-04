be260f3bbec82178eccadfe7b243934e25e25062
fisher310 <316049914@qq.com>
2020-04-04 16:08:59 +0800

    接雨水动态规划

--- src/main/java/p42/Solution.java
+++ src/main/java/p42/Solution.java
@@ -8,24 +8,21 @@ package p42;
  */
 class Solution {
   public int trap(int[] height) {
-    int[] left = new int[height.length];
-    int[] right = new int[height.length];
+    if (null == height || height.length == 0) return 0;
+    int len = height.length;
+    int[] left = new int[len];
+    int[] right = new int[len];
+    left[0] = height[0];
+    right[len - 1] = height[len - 1];
+    for (int i = 1, n = len; i < n; i++) {
+      left[i] = Math.max(height[i], left[i - 1]);
+      right[n - 1 - i] = Math.max(height[n - 1 - i], right[n - i]);
+    }
+
     int ans = 0;
-    for (int i = 1; i < height.length - 1; i++) {
-      left[i] = max(height, 0, i);
-      right[i] = max(height, i, height.length - 1);
+    for (int i = 1; i < len - 1; i++) {
       ans += Math.min(left[i], right[i]) - height[i];
     }
     return ans;
-  }
-
-  private int max(int[] height, int start, int end) {
-    int m = height[start];
-    for (int i = start + 1; i <= end; i++) {
-      if (height[i] > m) {
-        m = height[i];
-      }
-    }
-    return m;
   }
 }
