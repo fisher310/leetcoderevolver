e023b432d665a281370138cc1ce7ec411f104a06
fisher310 <316049914@qq.com>
2020-04-04 11:20:04 +0800

    N叉树Node

---
+++ src/main/java/util/multi/Node.java
@@ -0,0 +1,27 @@
+package util.multi;
+
+import java.util.List;
+
+public class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+
+    public static Node create(Integer[] nums) {
+
+        for (Integer n : nums) {
+
+        }
+        return null;
+    }
+}
