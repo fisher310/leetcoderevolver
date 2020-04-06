b429fd56f819ed6a4e00ab2bc301bd1c32870818
fisher310 <316049914@qq.com>
2020-04-06 13:47:06 +0800

    组合

---
+++ src/main/java/p77/Solution.java
@@ -0,0 +1,44 @@
+package p77;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 组合 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ *
+ * <p>示例:
+ *
+ * <p>输入: n = 4, k = 2 输出: [ [2,4], [3,4], [2,3], [1,2], [1,3], [1,4], ]
+ *
+ * <p>来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/combinations
+ */
+class Solution {
+  List<List<Integer>> ans = new LinkedList<>();
+
+  public List<List<Integer>> combine(int n, int k) {
+
+    int[] visited = new int[n + 1];
+    dfs(1, n, k, new LinkedList<>());
+    return ans;
+  }
+
+  void dfs(int j, int n, int k, List<Integer> list) {
+    if (list.size() == k) {
+      ans.add(new ArrayList<>(list));
+      return;
+    }
+    if (n - j + list.size() + 1 < k) return;
+    for (int i = j; i <= n; i++) {
+      list.add(i);
+      dfs(i + 1, n, k, list);
+      list.remove(list.size() - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    List<List<Integer>> combine = s.combine(4, 2);
+    System.out.println(combine);
+  }
+}
