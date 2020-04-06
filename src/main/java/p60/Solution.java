9a4dd9877a24b4ea337cac6719f9ed422fa2a798
fisher310 <316049914@qq.com>
2020-04-06 13:46:53 +0800

    第K个排列

--- src/main/java/p60/Solution.java
+++ src/main/java/p60/Solution.java
@@ -1,16 +1,34 @@
 package p60;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** 第K个排列 */
 class Solution {
   private int kth = 0;
   private String ans;
 
   public String getPermutation(int n, int k) {
-    kth = 0;
-    ans = "";
-    int[] visited = new int[n + 1];
-    dfs(n, k, visited, new StringBuilder());
-    return ans;
+    int[] factories = new int[n];
+    List<Integer> nums = new ArrayList<>();
+    nums.add(1);
+
+    factories[0] = 1;
+    for (int i = 1; i < n; i++) {
+      factories[i] = i * factories[i - 1];
+      nums.add(i + 1);
+    }
+
+    --k;
+    StringBuilder sb = new StringBuilder();
+    for (int i = n - 1; i > -1; i--) {
+      int idx = k / factories[i];
+      k -= idx * factories[i];
+      sb.append(nums.get(idx));
+      nums.remove(idx);
+    }
+
+    return sb.toString();
   }
 
   private void dfs(int n, int k, int[] visited, StringBuilder sb) {
