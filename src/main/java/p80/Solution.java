5c84491aa829202d8d9116d2191ac5a25a3222f3
fisher310 <316049914@qq.com>
2020-04-08 23:50:34 +0800

    删除排序数组中的重复项2

---
+++ src/main/java/p80/Solution.java
@@ -0,0 +1,49 @@
+package p80;
+
+import java.util.Arrays;
+
+/** 删除排序数组中的重复项2 */
+class Solution {
+  public int removeDuplicates(int[] nums) {
+    if (null == nums || nums.length == 0) return 0;
+
+    int N = nums.length;
+    int k = 0;
+    int pre = nums[0];
+    int c = 1;
+    for (int i = 1; i < N; i++) {
+      if (pre == nums[i]) {
+        c++;
+        if (c <= 2) {
+          k++;
+        }
+      } else {
+        c = 1;
+        pre = nums[i];
+        k++;
+      }
+      if (i != k) {
+        nums[k] = nums[i];
+      }
+    }
+    return k + 1;
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    int[] input = new int[] {1, 1, 1, 2, 2, 3};
+    int ans = s.removeDuplicates(input);
+    System.out.println(ans);
+    System.out.println(Arrays.toString(input));
+
+    int[] input2 = new int[] {0, 0, 1, 1, 1, 1, 2, 3, 3};
+    int ans2 = s.removeDuplicates(input2);
+    System.out.println(ans2);
+    System.out.println(Arrays.toString(input2));
+
+    int[] input3 = new int[] {1, 2, 3, 4, 5};
+    int ans3 = s.removeDuplicates(input3);
+    System.out.println(ans3);
+    System.out.println(Arrays.toString(input3));
+  }
+}
