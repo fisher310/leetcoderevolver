0dccfa96fd65a037a4ab70df88e389d406aea381
fisher310 <316049914@qq.com>
2020-04-09 22:08:43 +0800

    串联所有字符串

--- src/main/java/p30/Solution.java
+++ src/main/java/p30/Solution.java
@@ -21,89 +21,52 @@ import java.util.*;
  */
 class Solution {
 
-    private Set<Integer> ans = new HashSet<>();
-    private Map<String, List<Integer>> map = new HashMap<>();
+  private int wordLength;
 
-    public List<Integer> findSubstring(String s, String[] words) {
-        if (s == null || s.length() == 0 || words == null || words.length == 0)
-            return Collections.emptyList();
+  private int wordCount;
+  private Map<String, Integer> tmpMap;
 
-        int wordLength = words[0].length();
-        for (int i = 0; i < words.length; i++) {
-            if (map.containsKey(words[i])) {
-                map.get(words[i]).add(i);
-            } else {
-                List<Integer> l = new ArrayList<>();
-                l.add(i);
-                map.put(words[i], l);
-            }
-        }
-        int idx0 = s.indexOf(words[0]);
+  public List<Integer> findSubstring(String s, String[] words) {
+    if (s == null || s.length() == 0 || words == null || words.length == 0)
+      return Collections.emptyList();
 
-
-        outer:
-        for (; idx0 >= 0; idx0 = s.indexOf(words[0], idx0 + wordLength)) {
-            int l = idx0;
-            int r = idx0;
-            int[] visited = new int[words.length];
-            visited[0] = 1;
-            while ((l - wordLength) >= 0) {
-                l -= wordLength;
-                String ls = s.substring(l, l + wordLength);
-                List<Integer> wordIndexes = map.get(ls);
-                if (null == wordIndexes) {
-                    l += wordLength;
-                    break;
-                } else {
-                    int alreadyVisited = 0;
-                    for (int wordIndex : wordIndexes) {
-                        if (visited[wordIndex] == 1) {
-                            alreadyVisited++;
-                        } else {
-                            visited[wordIndex] = 1;
-                            break;
-                        }
-                    }
-                    if (alreadyVisited == wordIndexes.size()) {
-                        l += wordLength;
-                        break;
-                    }
-                }
-            }
-
-            while ((r + wordLength) < s.length()) {
-                r += wordLength;
-                String rs = s.substring(r, r + wordLength);
-                List<Integer> wordIndexes = map.get(rs);
-                if (null == wordIndexes) {
-                    r -= wordLength;
-                    break;
-                } else {
-                    int alreadyVisited = 0;
-                    for (int wordIndex : wordIndexes) {
-                        if (visited[wordIndex] == 1) {
-                            alreadyVisited++;
-                        } else {
-                            visited[wordIndex] = 1;
-                            break;
-                        }
-                    }
-                    if (alreadyVisited == wordIndexes.size()) {
-                        r -= wordLength;
-                        break;
-                    }
-                }
-            }
-
-            for (int vi : visited) {
-                if (vi == 0) {
-                    continue outer;
-                }
-            }
-            ans.add(l);
-        }
-
-        return new ArrayList<>(ans);
+    wordCount = words.length;
+    tmpMap = new HashMap<>(words.length);
+    Map<String, Integer> wordMap = new HashMap<>();
+    for (String word : words) {
+      wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
     }
 
+    List<Integer> ans = new LinkedList<>();
+    this.wordLength = words[0].length();
+    int totalLength = wordLength * words.length;
+    for (int i = 0, N = s.length(); i <= N - totalLength; i++) {
+      String sub = s.substring(i, i + totalLength);
+      if (contains(sub, wordMap)) {
+        ans.add(i);
+      }
+    }
+
+    return ans;
+  }
+
+  private boolean contains(String s, Map<String, Integer> wordMap) {
+    tmpMap.clear();
+    int tmpCount = 0;
+    for (int i = 0; i < s.length(); i += wordLength) {
+      String word = s.substring(i, i + wordLength);
+      if (wordMap.containsKey(word)) {
+        int cnt = tmpMap.getOrDefault(word, 0) + 1;
+        int totalCnt = wordMap.get(word);
+        if (cnt > totalCnt) {
+          return false;
+        }
+        tmpCount++;
+        tmpMap.put(word, cnt);
+      } else {
+        return false;
+      }
+    }
+    return tmpCount == wordCount;
+  }
 }
--- src/test/java/p30/SolutionTest.java
+++ src/test/java/p30/SolutionTest.java
@@ -6,14 +6,17 @@ import java.util.List;
 
 public class SolutionTest {
 
-    @Test
-    public void findSubstring() {
-        Solution s = new Solution();
-        List<Integer> ans = s.findSubstring("barfoothefoobarman", new String[]{"foo", "bar"});
-        System.out.println(ans);
+  @Test
+  public void findSubstring() {
+    Solution s = new Solution();
+    List<Integer> ans1 = s.findSubstring("barfoothefoobarman", new String[] {"foo", "bar"});
+    System.out.println(ans1);
 
-        s = new Solution();
-        ans = s.findSubstring("wordgoodgoodgoodbestword", new String[]{"word", "good", "best", "word"});
-        System.out.println(ans);
-    }
+    List<Integer> ans2 =
+        s.findSubstring("wordgoodgoodgoodbestword", new String[] {"word", "good", "best", "word"});
+    System.out.println(ans2);
+
+    System.out.println(
+        s.findSubstring("wordgoodgoodgoodbestword", new String[] {"word", "good", "best", "good"}));
+  }
 }
