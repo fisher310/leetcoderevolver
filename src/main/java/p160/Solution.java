577336d2619916759f740e3f0d96453538b7d870
fisher310 <316049914@qq.com>
2020-04-17 23:53:52 +0800

    相交链表

---
+++ src/main/java/p160/Solution.java
@@ -0,0 +1,18 @@
+package p160;
+
+import util.ListNode;
+
+/** 相交链表 */
+class Solution {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    if (headA == null || headB == null) return null;
+
+    ListNode pa = headA;
+    ListNode pb = headB;
+    while (pa != pb) {
+      pa = (pa == null) ? headB : pa.next;
+      pb = (pb == null) ? headA : pb.next;
+    }
+    return pa;
+  }
+}
