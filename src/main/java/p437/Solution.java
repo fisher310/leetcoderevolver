cb4867abe866056c695aab5a5940cee3b46054b3
fisher310 <316049914@qq.com>
2020-04-19 18:30:18 +0800

    最小覆盖子串

--- src/main/java/p437/Solution.java
+++ src/main/java/p437/Solution.java
@@ -2,52 +2,36 @@ package p437;
 
 import util.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
 
-    int ans = 0;
+  public int pathSum(TreeNode root, int sum) {
+    if (root == null) return 0;
 
-    public int pathSum(TreeNode root, int sum) {
-        dfs(new ArrayList<Node>(), root, sum, root);
-        return ans;
+    int res = 0;
+
+    res += findPath(root, sum);
+    res += pathSum(root.left, sum);
+    res += pathSum(root.right, sum);
+    return res;
+  }
+
+  private int findPath(TreeNode node, int sum) {
+    if (node == null) return 0;
+
+    int res = 0;
+    if (node.val == sum) {
+      res += 1;
     }
+    res += findPath(node.left, sum - node.val);
+    res += findPath(node.right, sum - node.val);
+    return res;
+  }
 
-    void dfs(List<Node> list, TreeNode node, int target, TreeNode parent) {
-        if (node == null) return;
-
-        dfs(list, node.left, target, node);
-        dfs(list, node.right, target, node);
-
-        int val = node.val;
-
-        if (val == target) ans++;
-
-        for (Node nd : list) {
-            if (nd.parent == node) {
-                nd.val += val;
-                nd.parent = parent;
-                if (nd.val == target) ans++;
-            }
-        }
-        list.add(new Node(val, parent));
-    }
-
-    static class Node {
-        int val;
-        TreeNode parent;
-
-        Node(int val, TreeNode parent) {
-            this.val = val;
-            this.parent = parent;
-        }
-    }
-
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int ans = s.pathSum(TreeNode.createTreeNode(new Integer[]{10, 5, -3, 3, 2, null, 11, 3, -2, null, 1}), 8);
-        System.out.println(ans);
-    }
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    int ans =
+        s.pathSum(
+            TreeNode.createTreeNode(new Integer[] {10, 5, -3, 3, 2, null, 11, 3, -2, null, 1}), 8);
+    System.out.println(ans);
+  }
 }
