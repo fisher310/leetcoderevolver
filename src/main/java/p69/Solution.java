65efd1ac3c5861e8e49bd4ee6d214a8b3258b37b
fisher310 <316049914@qq.com>
2020-04-19 23:07:11 +0800

    x 的平方根

--- src/main/java/p69/Solution.java
+++ src/main/java/p69/Solution.java
@@ -3,23 +3,32 @@ package p69;
 /** x 的平方根 */
 class Solution {
     public int mySqrt(int x) {
-        if (x == 1) return 1;
-        long l = 1;
-        long r = (long) x+1;
+        if (x == 0 || x == 1) return x;
 
-        while (l < r) {
-            long mid = l + (r - l) / 2;
-            long floor = mid * mid;
-            long ceil = (mid + 1) * (mid + 1);
-            if (floor <= x && x < ceil) {
-                return (int) mid;
-            } else if (ceil <= x) {
-                l = mid + 1;
-            } else {
-                r = mid;
+        double cur = 1;
+        while (true) {
+            double pre = cur;
+            cur = (cur + x/cur) /2;
+            if (Math.abs(cur - pre) < 1e-6) {
+                return (int) cur;
             }
         }
+//        long l = 1;
+//        long r = (long) x+1;
+//
+//        while (l < r) {
+//            long mid = l + (r - l) / 2;
+//            long floor = mid * mid;
+//            long ceil = (mid + 1) * (mid + 1);
+//            if (floor <= x && x < ceil) {
+//                return (int) mid;
+//            } else if (ceil <= x) {
+//                l = mid + 1;
+//            } else {
+//                r = mid;
+//            }
+//        }
 
-        return (int) l;
+//        return (int) l;
     }
 }
