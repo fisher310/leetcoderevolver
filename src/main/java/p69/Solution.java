0923f2d36656f728bc8eb085eb5bc9c4f845de71
fisher310 <316049914@qq.com>
2020-04-19 23:02:20 +0800

    x 的平方根

--- src/main/java/p69/Solution.java
+++ src/main/java/p69/Solution.java
@@ -4,11 +4,22 @@ package p69;
 class Solution {
     public int mySqrt(int x) {
         if (x == 1) return 1;
-        long i = 1;
-        while (i * i <= x) {
-            i++;
+        long l = 1;
+        long r = (long) x+1;
+
+        while (l < r) {
+            long mid = l + (r - l) / 2;
+            long floor = mid * mid;
+            long ceil = (mid + 1) * (mid + 1);
+            if (floor <= x && x < ceil) {
+                return (int) mid;
+            } else if (ceil <= x) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
         }
 
-        return (int) (i - 1);
+        return (int) l;
     }
 }
--- src/test/java/p69/SolutionTest.java
+++ src/test/java/p69/SolutionTest.java
@@ -9,6 +9,7 @@ public class SolutionTest {
     @Test
     public void mySqrt() {
         Solution s = new Solution();
+        assertEquals(46340,s.mySqrt(2147483647));
         assertEquals( 46340, s.mySqrt(2147395600));
         assertEquals(2, s.mySqrt(4));
         assertEquals(1, s.mySqrt(1));
