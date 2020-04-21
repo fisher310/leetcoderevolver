edcbb7d2a81b56ae5c895d34e408868b199012b3
fisher310 <316049914@qq.com>
2020-04-21 19:01:10 +0800

    优美子数组

--- src/main/java/p1248/Solution.java
+++ src/main/java/p1248/Solution.java
@@ -5,15 +5,14 @@ class Solution {
 
     public int numberOfSubarrays(int[] nums, int k) {
         int n = nums.length;
-        int[] cnt = new int[n+1];
-        int odd = 0, ans = 0;
+        int[] cnt = new int[n + 1];
         cnt[0] = 1;
+        int odd = 0, ans = 0;
         for (int num : nums) {
             odd += num & 1;
             ans += odd >= k ? cnt[odd - k] : 0;
             cnt[odd] += 1;
         }
-
         return ans;
     }
 }
