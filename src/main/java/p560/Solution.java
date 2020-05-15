abe0a6e24d9e91fcc23c57759a85a25044c4dcfa
fisher310 <316049914@qq.com>
2020-05-15 12:12:07 +0800

    前缀和

--- src/main/java/p560/Solution.java
+++ src/main/java/p560/Solution.java
@@ -1,18 +1,24 @@
 package p560;
 
-/** 和为K的子数组 */
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 和为K的子数组 <br>
+ * 前缀和的思路来解决问题
+ */
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int ans = 0;
-
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int pre = 0;
         for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    ans++;
-                }
+            pre += nums[i];
+            if (map.containsKey(pre - k)) {
+                ans += map.get(pre - k);
             }
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
         }
 
         return ans;
