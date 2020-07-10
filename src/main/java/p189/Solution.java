d9749d7dc30361b2e4e497233cbc326cac5466cb
fisher310 <316049914@qq.com>
2020-07-10 14:01:45 +0800

    数组旋转

--- src/main/java/p189/Solution.java
+++ src/main/java/p189/Solution.java
@@ -4,34 +4,32 @@ import java.util.Arrays;
 
 class Solution {
 
-  public void roate(int[] nums, int k) {
+  public void rotate(int[] nums, int k) {
     int N = nums.length;
 
     k = k % N;
 
-    int[] temp = new int[N];
-    System.arraycopy(nums, N - k, temp, 0, k);
-    //    for (int i = 0; i < k; i++) {
-    //      temp[i] = nums[N - k + i];
-    //    }
-
-    System.arraycopy(nums, 0, temp, k, N - k);
-    //    for (int i = k; i < N; i++) {
-    //      temp[i] = nums[i - k];
-    //    }
-    System.arraycopy(temp, 0, nums, 0, N);
+    reverse(nums, 0, N);
+    reverse(nums, 0, k);
+    reverse(nums, k, N);
   }
 
-  private void swap(int[] nums, int i, int j) {
-    int tmp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = tmp;
+  private void reverse(int[] nums, int start, int end) {
+    int l = start;
+    int r = end - 1;
+    while (l < r) {
+      int temp = nums[l];
+      nums[l] = nums[r];
+      nums[r] = temp;
+      l++;
+      r--;
+    }
   }
 
   public static void main(String[] args) {
     Solution s = new Solution();
     int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7};
-    s.roate(nums, 3);
+    s.rotate(nums, 3);
     System.out.println(Arrays.toString(nums));
   }
 }
