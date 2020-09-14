871f45e04b620ab195c6a19b1637fdfdfcbb26fd
fisher310 <316049914@qq.com>
2020-09-14 16:38:10 +0800

    bfs + dfs

--- src/main/java/p126/Solution.java
+++ src/main/java/p126/Solution.java
@@ -3,51 +3,170 @@ package p126;
 import java.util.*;
 
 class Solution {
+
+    int min = 1;
+
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
 
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return Collections.emptyList();
 
-        Queue<Integer> queue = new LinkedList<>();
+        Map<String, List<String>> map = bfs(beginWord, endWord, wordSet);
 
         List<String> res = new ArrayList<>();
-        List<List<String>> ans = new LinkedList<>();
+        List<List<String>> ans = new ArrayList<>();
 
         Set<String> visited = new HashSet<>();
-        for (String word : wordList) {
-            if (dis(beginWord, word)) {
-                visited.clear();
-                bfs(word, endWord, wordList, visited, new ArrayList<String>(), ans);
-            }
-        }
+        res.add(beginWord);
+        visited.add(beginWord);
+        dfs(beginWord, endWord, map, visited, res, ans);
 
         return ans;
     }
 
-    private void bfs(String word, String target, final List<String> wordList, Set<String> visited, List<String> res, List<List<String>> ans) {
+    private Map<String, List<String>> bfs(String beginWord, String endWord, Set<String> wordSet) {
+        Queue<String> queue = new LinkedList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        queue.offer(beginWord);
+        boolean found = false;
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
 
-        visited.add(word);
+        Set<String> levelSet = new HashSet<>();
 
-        for (String w : wordList) {
-            if (visited.contains(w)) {
-                continue;
+        while (!queue.isEmpty()) {
+            min++;
+            int size = queue.size();
+            for (int k = 0; k < size; k++) {
+                String word = queue.poll();
+                List<String> neighbors = new ArrayList<>();
+                char[] ch = word.toCharArray();
+                for (int i = 0; i < ch.length; i++) {
+                    char old = ch[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == old) continue;
+                        ch[i] = c;
+                        String tmp = new String(ch);
+                        if (tmp.equals(endWord)) found = true;
+                        if (wordSet.contains(tmp) && !visited.contains(tmp)) {
+                            neighbors.add(tmp);
+                            queue.offer(tmp);
+                            levelSet.add(tmp);
+                        }
+                    }
+                    ch[i] = old;
+                }
+                map.put(word, neighbors);
             }
+            if (found) break;
 
-            if (dis(word, w));
+            visited.addAll(levelSet);
+            levelSet.clear();
         }
-
-
+        return map;
     }
 
-    private boolean dis(String s1, String s2) {
-        int dis = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                dis++;
-                if (dis > 1) {
-                    return false;
-                }
+    private void dfs(
+            String beginWord,
+            String endWord,
+            Map<String, List<String>> map,
+            Set<String> visited,
+            List<String> res,
+            List<List<String>> ans) {
+        if (res.size() > min) return;
+        if (beginWord.equals(endWord)) {
+            if (res.size() < min) {
+                min = res.size();
+                ans.clear();
             }
+            ans.add(new ArrayList<>(res));
+            return;
         }
 
-        return dis == 1;
+        List<String> neighbors = map.get(beginWord);
+        if (neighbors == null) return;
+        for (String neighbor : neighbors) {
+            if (!visited.contains(neighbor)) {
+                visited.add(neighbor);
+                res.add(neighbor);
+                dfs(neighbor, endWord, map, visited, res, ans);
+                res.remove(res.size() - 1);
+                visited.remove(neighbor);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<List<String>> ans =
+                s.findLadders(
+                        "hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+        for (List<String> res : ans) {
+            System.out.println(res);
+        }
+
+        String beginWord = "cet", endWord = "ism";
+        String[] wordList =
+                new String[] {
+                    "kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip",
+                    "kay", "per", "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die",
+                    "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan", "cot",
+                    "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad",
+                    "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez", "own", "ump", "dis",
+                    "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe", "box",
+                    "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam",
+                    "flu", "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim",
+                    "san", "pan", "map", "ski", "ova", "wed", "non", "wac", "nut", "why", "bye",
+                    "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot",
+                    "bow", "fob", "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel",
+                    "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm", "lot", "tom",
+                    "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit",
+                    "doe", "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron",
+                    "soy", "gin", "don", "tug", "fay", "vic", "boo", "nam", "ave", "buy", "sop",
+                    "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod",
+                    "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie",
+                    "mon", "dog", "bib", "rub", "ere", "dig", "era", "cat", "fox", "bee", "mod",
+                    "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and", "ibm",
+                    "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx",
+                    "jog", "nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom",
+                    "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao", "mom", "mas",
+                    "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy",
+                    "rid", "bat", "sum", "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava",
+                    "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw", "vim", "sec", "ltd",
+                    "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw",
+                    "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any",
+                    "dow", "cod", "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao",
+                    "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass",
+                    "dem", "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen",
+                    "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia", "ewe", "hit", "fix",
+                    "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye",
+                    "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon",
+                    "roy", "dip", "hen", "iva", "lug", "asp", "hui", "yak", "bay", "poi", "yep",
+                    "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig",
+                    "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit",
+                    "hem", "joy", "bum", "rio", "yon", "dec", "leg", "put", "sue", "dim", "pet",
+                    "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc", "moe", "caw",
+                    "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt",
+                    "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil",
+                    "jig", "hub", "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig",
+                    "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two",
+                    "ins", "con", "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun",
+                    "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del", "imp", "cob", "way",
+                    "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah",
+                    "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was",
+                    "cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab",
+                    "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac",
+                    "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig",
+                    "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref",
+                    "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo",
+                    "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup",
+                    "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len",
+                    "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee",
+                    "pin", "dun", "pat", "ten", "mob"
+                };
+        List<List<String>> ladders = s.findLadders(beginWord, endWord, Arrays.asList(wordList));
+        for (List<String> ladder : ladders) {
+            System.out.println(ladder);
+        }
     }
 }
