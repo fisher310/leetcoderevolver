77b1cfce58b004df09089cbb4209429ec75d893f
fisher310 <316049914@qq.com>
2021-03-11 14:08:26 +0800

    add some solution

--- src/main/java/tmp/Test3.java
+++ src/main/java/tmp/Test3.java
@@ -3,28 +3,23 @@ package tmp;
 class Test3 {
 
     public int maxDiff(int[] arr) {
-        int l = 0, r = arr.length-1;
         int ans = 0;
-        while (l < r) {
-            int diff = arr[r] - arr[l];
-            if (diff > ans) {
-                ans = diff;
-                System.out.printf("arr[%d] - arr[%d] = %d\n", r, l, diff);
-            }
-
-            if (arr[r - 1] > arr[r]) {
-                r--;
+        int max = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
             } else {
-                l++;
+                ans = Math.max(ans, max - arr[i]);
             }
         }
-
         return ans;
     }
 
     public static void main(String[] args) {
         Test3 t = new Test3();
-        int ans = t.maxDiff(new int[]{662,660,449,456,890,340,190,570,789});
+        int ans = t.maxDiff(new int[] {662, 660, 449, 456, 890, 340, 190, 570, 789});
+        System.out.println(ans);
+        ans = t.maxDiff(new int[] {662, 660, 449, 160, 890, 340, 190, 570, 789});
         System.out.println(ans);
     }
 }
