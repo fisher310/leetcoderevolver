a32fee073ac7fd63c923322dcee96c31a4aa4df7
fisher310 <316049914@qq.com>
2021-04-20 09:36:53 +0800

    fork join test.

--- src/main/java/common/concurrent/ForkJoinTest.java
+++ src/main/java/common/concurrent/ForkJoinTest.java
@@ -1,5 +1,6 @@
 package common.concurrent;
 
+import java.net.SocketTimeoutException;
 import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
@@ -27,27 +28,37 @@ public class ForkJoinTest {
         @Override
         protected void compute() {
 
-            if (end - start < 10) {
+            if (end - start < 5000) {
                 for (int i = start; i < end; i++) {
                     array[i] += adder;
                 }
             } else {
                 int split = start + (end - start) / 2;
-                invokeAll(
-                        new AddFork(array, start, split, adder),
-                        new AddFork(array, split, end, adder));
+                new AddFork(array, start, split, adder).fork();
+                new AddFork(array, split, end, adder).fork();
             }
         }
     }
 
     public static void main(String[] args) {
-        ForkJoinPool pool = new ForkJoinPool();
+        ForkJoinPool pool = ForkJoinPool.commonPool();
 
-        int[] arr = new int[100];
+        int[] arr = new int[100000000];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
         }
+        long start = System.currentTimeMillis();
         pool.invoke(new AddFork(arr, 0, arr.length, 5));
-        System.out.println(Arrays.toString(arr));
+        long duration = System.currentTimeMillis() - start;
+
+        System.out.println("duration: " + duration);
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += 5;
+        }
+        duration = System.currentTimeMillis() - start;
+
+        System.out.println("loop duration: " + duration);
     }
 }
