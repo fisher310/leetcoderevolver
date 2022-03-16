50a08994c5fbd50895130c3c5027ee62bead96e9
lihailong <lihailong@58.com>
2022-03-16 12:40:56 +0800

    add p137

---
+++ src/main/java/p137/Solution.java
@@ -0,0 +1,28 @@
+package p137;
+
+import java.util.*;
+import org.junit.Assert;
+
+public class Solution {
+  public int singleNumber(int[] nums) {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      int v = map.getOrDefault(nums[i], 0) + 1;
+      map.put(nums[i], v);
+    }
+
+    for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+      if (e.getValue() == 1) {
+        return e.getKey();
+      }
+    }
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    Assert.assertEquals(3, s.singleNumber(new int[] {2, 2, 3, 2}));
+  }
+}
