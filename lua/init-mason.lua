00571ed1431866ab3ddd679c87ffade3c5d5384a
lihailong <lihailong@58.com>
2023-02-13 20:02:23 +0800

    remove lua

--- lua/init-mason.lua
+++ lua/init-mason.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
-    'sumneko_lua',
+    -- 'sumneko_lua',
     'tsserver',
     'graphql',
     'rust_analyzer',
