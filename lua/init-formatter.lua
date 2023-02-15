e28355a5bc7d53840f0fa1cfbb6199397ca37844
fisher310 <316049914@qq.com>
2023-02-15 15:19:58 +0800

    add fixjson

--- lua/init-formatter.lua
+++ lua/init-formatter.lua
@@ -1,28 +1,31 @@
 -- Utilities for creating configurations
-local util = require "formatter.util"
+local util = require("formatter.util")
 
 -- Provides the Format and FormatWrite commands
-require('formatter').setup {
-  -- All formatter configurations are opt-in
-  filetype = {
-    lua = {
-      -- Pick from defaults:
-      require('formatter.filetypes.lua').stylua,
+require("formatter").setup({
+	-- All formatter configurations are opt-in
+	filetype = {
+		lua = {
+			-- Pick from defaults:
+			require("formatter.filetypes.lua").stylua,
 
-      -- ,or define your own:
-      function()
-        return {
-          exe = "stylua",
-          args = {
-            "--search-parent-directories",
-            "--stdin-filepath",
-            util.escape_path(util.get_current_buffer_file_path()),
-            "--",
-            "-",
-          },
-          stdin = true,
-        }
-      end
-    }
-  }
-}
+			-- ,or define your own:
+			function()
+				return {
+					exe = "stylua",
+					args = {
+						"--search-parent-directories",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+						"--",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
+		json = {
+			require("formatter.filetypes.json").fixjson,
+		},
+	},
+})
